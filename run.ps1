78e67fe61f2bcbfd26658732015e99b23b13613a
Jasper van Merle <jaspervmerle@gmail.com>
2022-10-01 17:58:40 +0200

    Rename repositories/mastersofjava to repositories/server

--- run.ps1
+++ run.ps1
@@ -4,10 +4,10 @@ if (Test-Path -Path "repositories/assignments") {
     git clone https://github.com/First8/mastersofjava.git repositories/assignments
 }
 
-if (Test-Path -Path "repositories/mastersofjava") {
-    git -C repositories/mastersofjava pull
+if (Test-Path -Path "repositories/server") {
+    git -C repositories/server pull
 } else {
-    git clone https://github.com/mastersofjava/mastersofjava.git repositories/mastersofjava
+    git clone https://github.com/mastersofjava/mastersofjava.git repositories/server
 }
 
 docker compose up $args
--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ else
     git clone https://github.com/First8/mastersofjava.git repositories/assignments
 fi
 
-if [[ -d repositories/mastersofjava ]]; then
-    git -C repositories/mastersofjava pull
+if [[ -d repositories/server ]]; then
+    git -C repositories/server pull
 else
-    git clone https://github.com/mastersofjava/mastersofjava.git repositories/mastersofjava
+    git clone https://github.com/mastersofjava/mastersofjava.git repositories/server
 fi
 
 docker compose up "$@"
